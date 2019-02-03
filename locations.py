0c555b6e9912b6279616d5bb88a0ef0218e427e6
Sylvie <wisepoison@gmail.com>
2019-02-03 02:22:26 +0000

    optimizations

--- locations.py
+++ locations.py
@@ -5,12 +5,19 @@ from __future__ import division
 from datetime import datetime
 from collections import namedtuple, defaultdict
 from itertools import takewhile
-import json
 import argparse
 import csv
+import calendar
 
+import ijson.backends.yajl2_cffi as ijson
 from geopy.distance import geodesic
 import geocoder
+
+# worthwhile optimizations:
+# - use the raw timestamps instead of dates
+# - instead of geodeisc, use a bounding box like
+#   https://github.com/jfein/PyGeoTools/blob/master/geolocation.py
+
 
 # "home" "home address" 0.5mi "work" "work address 0.5mi --from "0/1/1" --to ..
 # -> date, home, work\n11/1/1,True,False
@@ -22,69 +29,88 @@ Addr = namedtuple("Addr", "latitude, longitude, name, dist")
 def valid_date(s):
     "argparse date type validation"
     try:
-        return datetime.strptime(s, "%Y-%m-%d")
+        return datetime.strptime(s, "%Y-%m-%d").date()
     except ValueError:
         msg = "Not a valid date: '{0}'.".format(s)
         raise argparse.ArgumentTypeError(msg)
 
+
+parser = argparse.ArgumentParser(
+    description="says which days you were near which addresses"
+)
+parser.add_argument(
+    "-a", "--addr", "--address",
+    nargs=3,
+    action="append",
+    dest="addrs",
+    metavar=("address-name", "address", "distance from address (km)")
+)
+parser.add_argument("--start", type=valid_date, default=valid_date("2000-1-1"))
+parser.add_argument("--stop", required=False, type=valid_date, default=datetime.now().date())
+parser.add_argument("-d", "--debug", action="store_true")
+parser.add_argument("-o", "--output", default="locations.csv")
+args = parser.parse_args()
+
+if args.debug:
+    print("looking at addresses {} from {} to {}".format(
+        repr(args.addrs),
+        args.start,
+        args.stop
+    ))
+    from pdb import pm
+
+
+addrs = []
+for addr in args.addrs:
+    place = geocoder.osm(addr[1])
+    addrs.append(Addr(place.lat, place.lng, addr[0], float(addr[2])))
+
+# depricated, kept for testing purposes
 def read_points():
-    "../Location History.json -> Point generator"
     with open("../Location History.json") as f:
-        data = json.load(f)
+        for point in ijson.items(f, "locations.item"):
+            yield Point(
+                point["latitudeE7"] / 10 ** 7,
+                point["longitudeE7"] / 10 ** 7,
+                datetime.fromtimestamp(int(point["timestampMs"]) / 1000).date()
+            )
 
-    for point in data["locations"]:
-        yield Point(
-            point["latitudeE7"] / 10 ** 7,
-            point["longitudeE7"] / 10 ** 7,
-            datetime.fromtimestamp(int(point["timestampMs"]) / 1000).date()
-        )
+with open(args.output, "w") as o:
+    out = csv.writer(o)
+    out.writerow(["date"] + [addr.name for addr in addrs])
 
-def main():
-    parser = argparse.ArgumentParser(
-        description="says which days you were near which addresses"
-    )
-    parser.add_argument(
-        "--addr", "-a",
-        nargs=3,
-        action="append",
-        dest="addrs",
-        metavar=("address-name", "address", "distance from address (km)")
-    )
-    parser.add_argument("--start", type=valid_date, default=valid_date("2000-1-1"))
-    parser.add_argument("--stop", required=False, default=datetime.now())
-    parser.add_argument("-d", "--debug", action="store_true")
-    args = parser.parse_args()
+    day = [args.stop] + [False for addr in addrs]
+    n = 0
 
     if args.debug:
-        print("looking at addresses {} from {} to {}".format(
-            repr(args.addrs),
-            args.start,
-            args.stop
-        ))
+        print "starting points crunching"
 
-    addrs = []
-    for addr in args.addrs:
-        place = geocoder.osm(addr[1])
-        addr.append(Addr(place.lat, place.lng, addr[0], addr[2]))
+    rows = []
+    with open("../Location History.json") as f:
+        for point in ijson.items(f, "locations.item"):
+            point = Point(
+                point["latitudeE7"] / 10 ** 7,
+                point["longitudeE7"] / 10 ** 7,
+                datetime.fromtimestamp(int(point["timestampMs"]) / 1000).date()
+            )
+            # it goes from most recent (large values) to oldest (small values)
+            if point.date > args.stop:
+                continue
+            if point.date < args.start:
+                break
+            if day[0] > point.date:
+                rows.append(day)
+                if args.debug:
+                    print "wrote {} with {} points".format(
+                        day[0].strftime("%Y-%m-%d"),
+                        n
+                    )
+                day = [point.date] + [False for addr in addrs]
+                n = 0
+            for i, addr in enumerate(addrs):
+                if geodesic(addr[:2], point[:2]).km <= addr.dist:
+                    day[i + 1] = True
+                    continue
+            n += 1
+    out.writerows(reversed(rows))
 
-    points = takewhile(
-        lambda point: args.start <= point.datetime <= args.stop,
-        read_points()
-    )
-
-    days = defaultdict(lambda : {addr.name: False for addr in addrs}, {})
-
-    for point in points:
-        for addr in addrs:
-            if geodesic(addr[:2], point[:2]).km <= addr.dist:
-                days[addr.date][addr.name] = True
-
-    with open("locations.csv", "w") as f:
-        csv.writer(f).writerows(
-            [["date"] + [addr.name for addr in addrs]] +
-            [[date] + [values[addr.name] for addr in addrs]
-             for date, values in sorted(days.items())]
-        )
-
-if __name__ == "__main__":
-    main()
